c56abf793ee33d1db463190d121c59b2bf9e073f
lancy <lancy1014@gmail.com>
2012-03-14 16:52:38 +0800

    a beginning

---
+++ IPVA/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  IPVA
+//
+//  Created by Lancy on 12-3-14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ IPVA/TrendViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrendViewController.h
+//  IPVA
+//
+//  Created by Lancy on 12-3-14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrendViewController : UIViewController
+
+@end
