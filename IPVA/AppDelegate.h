fd0668996583839895abfd78cbf013a15cc0dbd2
Lancy <lancy@iMac51matoiMac.local>
2012-03-14 16:24:22 +0800

    Initial Commit

---
+++ IPVA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IPVA
+//
+//  Created by Lancy on 12-3-14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ IPVA/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  IPVA
+//
+//  Created by Lancy on 12-3-14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
